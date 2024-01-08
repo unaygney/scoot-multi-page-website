8a58c7bf99f7e35c3ee0b6821a5b469f0d1aa150
Guney Unay <guneymarketing17@hotmail.com>
2024-01-08 12:27:14 +0300

    button component styles is done 🎨

--- components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ title, onClick, ...rest }) {
-  return (
-    <button onClick={onClick} className={`${className} `} {...rest}>
-      {title}
-    </button>
-  );
-}
